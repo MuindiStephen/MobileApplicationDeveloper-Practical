ba8c3012e491c839a69f241daf8bdee86ae0919c
MuindiStephen <stephenmuindi241@gmail.com>
2025-11-01 04:01:46 +0300

    Added auth dashboard for signup/signin selection

---
+++ app/src/main/java/com/muindi/stephen/mobiledeveloperpractical/ui/fragments/auth/AuthDashboardFragment.kt
@@ -0,0 +1,44 @@
+package com.muindi.stephen.mobiledeveloperpractical.ui.fragments.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.muindi.stephen.mobiledeveloperpractical.R
+import com.muindi.stephen.mobiledeveloperpractical.databinding.FragmentAuthDashboardBinding
+
+
+class AuthDashboardFragment : Fragment() {
+    private lateinit var binding: FragmentAuthDashboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {  }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAuthDashboardBinding.inflate(
+            inflater, container, false
+        )
+
+        binding.apply {
+            buttonSignIn.setOnClickListener {
+                findNavController().navigate(
+                    R.id.action_authDashboardFragment_to_loginFragment
+                )
+            }
+            buttonSignUp.setOnClickListener {
+                findNavController().navigate(
+                    R.id.action_authDashboardFragment_to_signUpFragment
+                )
+            }
+        }
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/muindi/stephen/mobiledeveloperpractical/ui/fragments/splash/SplashFragment.kt
+++ app/src/main/java/com/muindi/stephen/mobiledeveloperpractical/ui/fragments/splash/SplashFragment.kt
@@ -42,7 +42,7 @@ class SplashFragment : Fragment() {
         lifecycleScope.launch {
             delay(1500L)
             findNavController().navigate(
-                R.id.action_splashFragment_to_loginFragment
+                R.id.action_splashFragment_to_authDashboardFragment
             )
         }
     }
