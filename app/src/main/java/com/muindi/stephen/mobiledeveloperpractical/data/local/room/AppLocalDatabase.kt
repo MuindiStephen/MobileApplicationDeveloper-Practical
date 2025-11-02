26603b138dc227c7ac57114e704eeaf4fee3ec29
MuindiStephen <stephenmuindi241@gmail.com>
2025-11-02 04:07:37 +0300

    Updated Applocal room database with new entities

--- app/src/main/java/com/muindi/stephen/mobiledeveloperpractical/data/local/room/AppLocalDatabase.kt
+++ app/src/main/java/com/muindi/stephen/mobiledeveloperpractical/data/local/room/AppLocalDatabase.kt
@@ -2,18 +2,28 @@ package com.muindi.stephen.mobiledeveloperpractical.data.local.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.muindi.stephen.mobiledeveloperpractical.data.local.room.dao.GeneralAssessmentDao
+import com.muindi.stephen.mobiledeveloperpractical.data.local.room.dao.OverweightAssessmentDao
 import com.muindi.stephen.mobiledeveloperpractical.data.local.room.dao.PatientRegistrationDao
 import com.muindi.stephen.mobiledeveloperpractical.data.local.room.dao.VitalDao
 import com.muindi.stephen.mobiledeveloperpractical.data.model.requests.patients.PatientRegistrationRequest
+import com.muindi.stephen.mobiledeveloperpractical.data.model.requests.visits.VisitsGeneralAssessmentRequest
+import com.muindi.stephen.mobiledeveloperpractical.data.model.requests.visits.VisitsOverweightAssessmentRequest
 import com.muindi.stephen.mobiledeveloperpractical.data.model.requests.vitals.AddVitalRequest
 
 
 @Database(
-    entities = [PatientRegistrationRequest::class, AddVitalRequest::class],
+    entities = [PatientRegistrationRequest::class,
+        AddVitalRequest::class,
+        VisitsGeneralAssessmentRequest::class,
+        VisitsOverweightAssessmentRequest::class,
+               ],
     version = 1,
     exportSchema = false
 )
 abstract class AppLocalDatabase : RoomDatabase() {
     abstract fun patientRegistrationDao(): PatientRegistrationDao
     abstract fun vitalDao(): VitalDao
+    abstract fun visitsGeneralAssessmentInformationDao(): GeneralAssessmentDao
+    abstract fun visitsOverweightAssessmentInformationDao(): OverweightAssessmentDao
 }
--- app/src/main/java/com/muindi/stephen/mobiledeveloperpractical/data/local/room/dao/GeneralAssessmentDao.kt
+++ app/src/main/java/com/muindi/stephen/mobiledeveloperpractical/data/local/room/dao/GeneralAssessmentDao.kt
@@ -3,7 +3,6 @@ package com.muindi.stephen.mobiledeveloperpractical.data.local.room.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-import com.muindi.stephen.mobiledeveloperpractical.data.model.requests.patients.PatientRegistrationRequest
 import com.muindi.stephen.mobiledeveloperpractical.data.model.requests.visits.VisitsGeneralAssessmentRequest
 
 @Dao
